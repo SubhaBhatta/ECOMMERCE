5d1fd1d913b160f75803795bf1368a4a6f63e4d5
SubhaBhatta <subhabhatta3@gmail.com>
2025-05-11 16:10:54 +0545

    About Page Updated

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "../../assets/css/about.css";
 
-function about() {
+function About() {
   return (
-    <>
-      <p>THIS IS A E-COMMERCE</p>
-    </>
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to MySite! We're a team of passionate developers building modern
+        web experiences with creativity and performance in mind.
+      </p>
+      <p>
+        Our mission is to deliver high-quality, user-friendly applications that
+        solve real-world problems and bring value to our users.
+      </p>
+      <p>
+        Whether you're browsing our shop, contacting us, or just exploring,
+        we're glad you're here. Thank you for supporting us!
+      </p>
+    </div>
   );
 }
 
-export default about;
+export default About;
